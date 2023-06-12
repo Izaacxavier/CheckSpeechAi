11a139323bf96ddcbd5b42f2762adc7c4f4ec034
Izaac xavier <izaacxavier@gmail.com>
2023-06-12 15:58:18 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ["styled-components"],
+    },
+  },
+  resolve: {
+    alias: {
+      'styled-components': path.resolve(__dirname, 'node_modules', 'styled-components'),
     },
   },
 })
